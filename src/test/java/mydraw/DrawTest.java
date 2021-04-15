67f127ec061cc1d03aec57b1ddf7348ba488b711
18bajusz <8bajusz@informatik.uni-hamburg.de>
2021-04-15 13:45:40 +0200

    [DrawTest] - add getHeightTest - add setHeightTest - add getWidthTest - add setWidthTest

--- src/test/java/mydraw/DrawTest.java
+++ src/test/java/mydraw/DrawTest.java
@@ -6,10 +6,30 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DrawTest {
+    // init Draw and GUI
+    Draw DrawTest = new Draw();
+    DrawGUI DrawTestGUI = new DrawGUI(DrawTest);
+
     @Test
-    void test1() {
-        Draw DrawTest = new Draw();
-        Assertions.assertEquals(1, DrawTest.window.getHeight());
+    void getHeightTest() {
+        Assertions.assertEquals(400, DrawTestGUI.getHeight());
+    }
+
+    @Test
+    void setHeightTest() {
+        DrawTestGUI.setHeight(800);
+        Assertions.assertEquals(800, DrawTestGUI.getHeight());
+    }
+
+    @Test
+    void getWidthTest() {
+        Assertions.assertEquals(500, DrawTestGUI.getWidth());
+    }
+
+    @Test
+    void setWidthTest() {
+        DrawTestGUI.setWidth(800);
+        Assertions.assertEquals(800, DrawTestGUI.getWidth());
     }
 }
 
