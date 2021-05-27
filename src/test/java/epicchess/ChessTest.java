9dcf8c7c4f3dea0700af88f8f091589117233f6b
Theodor Bajusz <theodor.bajusz@baqend.com>
2021-05-27 10:32:42 +0200

    [ChessTest] - add ChessTest

---
+++ src/test/java/epicchess/ChessTest.java
@@ -0,0 +1,14 @@
+package epicchess;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class ChessTest {
+    // TODO init Chessboard etc.
+
+    @Test
+    void getHeightTest() {
+        Assertions.assertTrue(true);
+    }
+}
