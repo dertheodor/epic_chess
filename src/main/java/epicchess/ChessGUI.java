4e912dd352e8b26f12849cdcf7cc7eef6402d451
Theodor Bajusz <theodor.bajusz@baqend.com>
2021-05-27 11:52:31 +0200

    [ChessGUI] - larger window - change position of a-h to be on the bottom - disable a-h and 1-8 buttons

--- src/main/java/epicchess/ChessGUI.java
+++ src/main/java/epicchess/ChessGUI.java
@@ -26,7 +26,7 @@ public class ChessGUI {
 
         //Initialise Window
         gameUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameUI.setSize(500, 500);
+        gameUI.setSize(1200, 1000);
         gameUI.setLayout(new BorderLayout());
 
         //Add MenuBar to our Window
@@ -34,8 +34,12 @@ public class ChessGUI {
         gameUI.setJMenuBar(menuBar);
 
         //Initialise board
-        darkTileColor = new Color(115, 77, 38);
-        lightTileColor = new Color(217, 177, 140);
+        darkTileColor = new Color(118, 150, 86);
+        lightTileColor = new Color(238, 238, 210);
+
+        // TODO grey board option
+        //darkTileColor = new Color(125, 135, 150);
+        //lightTileColor = new Color(232, 235, 239);
 
         //Set layout
         board.setLayout(new GridLayout(0, 9));
@@ -43,11 +47,18 @@ public class ChessGUI {
         //Fill board
         for (int i = 0; i < 81; i++) {
             JButton newButton = new JButton();
-            if (i < 10 || i % 9 == 0)
+            if (i > 72 || i % 9 == 0) {
                 descriptionOfEdgeTiles(i, newButton);
-            else {
+                // make edge-buttons un-clickable
+                newButton.setEnabled(false);
+                // set edge-buttons size
+                newButton.setFont(new Font("Arial Unicode MS", Font.BOLD, 30));
+            } else {
                 if (i % 2 == 0) {
                     newButton.setBackground(darkTileColor);
+                    // TODO init board with correct pieces
+                    newButton.setFont(new Font("Arial Unicode MS", Font.BOLD, 90));
+                    newButton.setText("\u2655");
                 } else {
                     newButton.setBackground(lightTileColor);
                 }
@@ -69,55 +80,55 @@ public class ChessGUI {
     private void descriptionOfEdgeTiles(int i, JButton button) {
         switch (i) {
             case 0:
-                button.setText("");
-                break;
-            case 1:
-                button.setText("a");
-                break;
-            case 2:
-                button.setText("b");
-                break;
-            case 3:
-                button.setText("c");
-                break;
-            case 4:
-                button.setText("d");
-                break;
-            case 5:
-                button.setText("e");
-                break;
-            case 6:
-                button.setText("f");
-                break;
-            case 7:
-                button.setText("g");
-                break;
-            case 8:
-                button.setText("h");
+                button.setText("8");
                 break;
             case 9:
-                button.setText("1");
+                button.setText("7");
                 break;
             case 18:
-                button.setText("2");
+                button.setText("6");
                 break;
             case 27:
-                button.setText("3");
+                button.setText("5");
                 break;
             case 36:
                 button.setText("4");
                 break;
             case 45:
-                button.setText("5");
+                button.setText("3");
                 break;
             case 54:
-                button.setText("6");
+                button.setText("2");
                 break;
             case 63:
-                button.setText("7");
+                button.setText("1");
                 break;
             case 72:
-                button.setText("8");
+                button.setText("");
+                break;
+            case 73:
+                button.setText("a");
+                break;
+            case 74:
+                button.setText("b");
+                break;
+            case 75:
+                button.setText("c");
+                break;
+            case 76:
+                button.setText("d");
+                break;
+            case 77:
+                button.setText("e");
+                break;
+            case 78:
+                button.setText("f");
+                break;
+            case 79:
+                button.setText("g");
+                break;
+            case 80:
+                button.setText("h");
                 break;
             default:
         }
